9fdda02fcbf8446272df90055d041905654be6e3
Suriya Priyan <suriyapriyan0506@gmail.com>
2026-07-13 10:30:17 +0530

    update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://admission-management-crm-backend.vercel.app/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://admission-management-crm-backend-2.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
